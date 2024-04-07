03cdbdbaebd3bde6a2945e99b584b5ce265e0415
Ilja Gamza <51061669+TheSnak7@users.noreply.github.com>
2024-04-07 03:35:19 +0200

    AOC Day 4 - Part 2 solved

--- day4/day4.go
+++ day4/day4.go
@@ -9,9 +9,10 @@ import (
 )
 
 func Day4() int {
-	return sumWinningNumbers("../day4/input.txt")
+	return calculateTotalScratchcards("../day4/input.txt")
 }
 
+// Part 1
 func sumWinningNumbers(path string) int {
 	file, err := os.Open(path)
 
@@ -46,6 +47,65 @@ func sumWinningNumbers(path string) int {
 	}
 
 	return sum
+}
+
+// Part 2
+func calculateTotalScratchcards(path string) int {
+	file, err := os.Open(path)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	//Assume at least one card is always present
+	cardIndex := 0
+	matches := make([]int, 0)
+
+	for scanner.Scan() {
+		card := scanner.Text()
+		cardIndex++
+
+		winnningNums, chosenNums := parseCard(card)
+
+		matched := 0
+
+		//Assume all numbers are unique on each side
+		for _, wN := range winnningNums {
+			for _, cN := range chosenNums {
+				if cN == wN {
+					matched++
+				}
+			}
+		}
+		matches = append(matches, matched)
+	}
+
+	cardsWon := 0
+
+	cards := make([]int, len(matches))
+
+	for i := range cards {
+		cards[i] = 1
+	}
+
+	for i, matchCount := range matches {
+		//fmt.Printf("Card %v (%v) has %v matches\n", i+1, cards[i], matchCount)
+		cardCount := cards[i]
+		cardsWon += cardCount
+
+		for j := range matchCount {
+			//Bounds check
+			if j+i < len(cards) {
+				cards[j+i+1] += cardCount
+				//fmt.Printf("  Copy won of Card %v\n", j+i+2)
+			}
+		}
+	}
+
+	return cardsWon
 }
 
 func parseCard(card string) ([]int, []int) {
